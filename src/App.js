c1ac15847e649adaef65c9bff134a9e6d58e1a92
Malachi Grant <malachimgrant@gmail.com>
2019-02-25 23:28:03 -0400

    Added everything

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: 100,
+      percent: 1
+    };
+  }
+
+  startChanged = (e) => {
+    this.setState({start: e.target.value});
+  }
+  percentChanged = (e) => {
+    this.setState({percent: e.target.value});
+  }
+
   render() {
+    const percent = this.state.percent * 0.01 + 1;
+    const payment = this.state.start * Math.pow(percent, 12)/(Math.pow(percent, 9) + Math.pow(percent, 6) + Math.pow(percent, 3) + 1);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{marginTop: '50px'}}>
+        <span>Starting balance: </span><input type='number' value={this.state.start} onChange={this.startChanged}></input>
+        <br/>
+        <span>Percent interest: </span><input type='number' value={this.state.percent} onChange={this.percentChanged}></input>
+        <br/>
+        <span>{'Quarterly payment: ' + payment}</span>
       </div>
     );
   }
